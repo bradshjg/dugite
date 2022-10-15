cfb3b3ee2e342ae91d1de570d785dc2824e0be9c
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2022-10-15 15:32:27 -0500

    Add dummy program for testing

---
+++ examples/status/index.js
@@ -0,0 +1,30 @@
+import { GitProcess } from 'dugite'
+
+const git = GitProcess
+
+async function checkStatusExec(path) {
+  console.log(`Checking status of ${path} with exec`)
+  const result = await git.exec(['status'], path)
+  console.log(result.stdout)
+}
+
+async function checkStatusSpawn(path) {
+  console.log(`Checking status of ${path} with spawn`)
+  const process = git.spawn(['status'], path)
+  process.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  process.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  process.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
+
+const path = process.argv[2]
+console.log(`Using path: ${path}\n`)
+// await checkStatusExec(path)
+await checkStatusSpawn(path)
